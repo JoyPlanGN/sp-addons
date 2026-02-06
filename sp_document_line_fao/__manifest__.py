4407ec9373de3c51986ffdd4261dbd812f493b76
Giuseppe <giuseppe.nitto@joyplan.it>
2026-02-06 09:13:20 +0100

    ADD: sp_document_line_fao module skeleton

---
+++ sp_document_line_fao/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "SP - FAO su righe documenti",
+    "version": "19.0.1.0.0",
+    "category": "Inventory/Purchase",
+    "summary": "Snapshot FAO sulle righe di acquisto e movimenti magazzino",
+    "author": "Salerno Pesca / JoyPlan",
+    "license": "LGPL-3",
+    "depends": [
+        "purchase",
+        "stock",
+        "sp_lot_fao",
+    ],
+    "data": [
+        # Views verranno aggiunte nello Step successivo
+    ],
+    "installable": True,
+    "application": False,
+}
